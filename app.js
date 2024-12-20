2154935096020b95eff83fb080be44179f1d81cd
mirziyo <mirziyo>
2024-12-20 15:15:52 +0500

    JSmisollar

---
+++ app.js
@@ -0,0 +1,121 @@
+//1 misol
+function sum(x, y) {
+  return x + y;
+}
+
+sum(3, 2);
+
+//2 misol
+
+function toSekunds(minutes) {
+  return minutes * 60;
+}
+
+// 3misol
+
+function nextInteger(int) {
+  return int + 1;
+}
+
+// 4 misol
+
+function uchburchakYuzi(asos, balandlik) {
+  let s = (asos * balandlik) / 2;
+
+  return s;
+}
+
+// 5 misol
+
+function ageToDays(age) {
+  const days = age * 365;
+  return days;
+}
+
+//6 misol
+
+function kub(number) {
+  return number ** 3;
+}
+
+//7 misol
+
+function firstElement(arr) {
+  return arr[0];
+}
+
+let arr = [1, 2, 3];
+
+firstElement(arr);
+
+// 8 misol
+
+function power(x, y) {
+  return x * y;
+}
+
+power(1, 3);
+
+//9 misol
+
+function hourToSekunds(hour) {
+  return hour * 60;
+}
+
+hourToSekunds(2);
+
+//10 misol
+
+function uchinchiTomon(tomon1, tomon2) {
+  return (tomon = tomon1 + tomon2 - 1);
+}
+
+// 11 misol
+
+function qoldiq(son1, son2) {
+  return son1 % son2;
+}
+
+//12 misol
+
+function turtburchakYuzi(boyi, eni) {
+  return (s = boyi * eni);
+}
+
+// 13 misol
+
+function stringQoshish(a) {
+  return "something" + a;
+}
+
+stringQoshish("is better than nothing");
+
+//14 misol
+
+function kvadirat(number) {
+  return number ** 2;
+}
+
+kvadirat(5);
+
+//15 misol
+
+function noldan(raqam) {
+  if (raqam <= 0) {
+    return "true";
+  } else {
+    return "fulse";
+  }
+}
+
+noldan(0);
+
+// 16 misol
+
+function ichkiBurchaklar(n) {
+  return (n - 2) * 180;
+}
+
+ichkiBurchaklar(4);
+
+
